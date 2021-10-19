25072e7887bbc89603b522da6a21ccd3ae31b55f
Ilya Stasialovich <i.stasialovich.vironit@vironit.ru>
2021-10-19 14:57:52 +0300

    add SettingsAdapter

--- app/src/main/java/com/example/currencynb/ui/fragments/CurrencyFragment.kt
+++ app/src/main/java/com/example/currencynb/ui/fragments/CurrencyFragment.kt
@@ -6,13 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.currencynb.R
 import com.example.currencynb.adapter.CurrencyAdapter
 import com.example.currencynb.base.BaseFragment
 import com.example.currencynb.databinding.FragmentCurrencyBinding
 import com.example.currencynb.other.Resource
 import com.example.currencynb.ui.MainActivity
 import com.example.currencynb.ui.ViewModelCurrency
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CurrencyFragment : BaseFragment<FragmentCurrencyBinding>() {
@@ -27,7 +32,14 @@ class CurrencyFragment : BaseFragment<FragmentCurrencyBinding>() {
         super.onViewCreated(view, savedInstanceState)
         viewModelCurrency = (activity as MainActivity).viewModelCurrency
         initAdapter()
-        viewModelCurrency.itemsCurrency().observe(viewLifecycleOwner, Observer { // вешаем наблюдателя на наши breackingNews данные во ViewModel
+        time()
+        binding.ivSettings.setOnClickListener {
+            findNavController().navigate(R.id.action_currencyFragment_to_settingsFragment)
+        }
+        binding.ivLeft.setOnClickListener {
+            finishAffinity(requireActivity())
+        }
+        viewModelCurrency.itemsCurrency().observe(viewLifecycleOwner, {
                 response ->
             when(response){
                 is Resource.Success ->{
@@ -55,11 +67,19 @@ class CurrencyFragment : BaseFragment<FragmentCurrencyBinding>() {
     private fun hideProgressBar() {
         binding.progressBar.visibility = View.INVISIBLE
     }
+
     private fun showProgressBar() {
         binding.progressBar.visibility = View.VISIBLE
     }
+
     private fun initAdapter() {
         currencyAdapter = CurrencyAdapter()
         binding.rvCurrency.adapter = currencyAdapter
+    }
+
+    private fun time() {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+        binding.tvTimeNow.text = dateFormat.format(calendar.time)
     }
 }
--- app/src/main/java/com/example/currencynb/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/currencynb/ui/fragments/SettingsFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.currencynb.R
+import com.example.currencynb.adapter.CurrencyAdapter
+import com.example.currencynb.adapter.SettingsAdapter
 import com.example.currencynb.base.BaseFragment
 import com.example.currencynb.databinding.FragmentSettingsBinding
+import com.example.currencynb.other.Resource
+import com.example.currencynb.ui.MainActivity
+import com.example.currencynb.ui.ViewModelCurrency
 
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
+    lateinit var settingsAdapter: SettingsAdapter
+    lateinit var viewModelCurrency: ViewModelCurrency
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -17,6 +28,42 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModelCurrency = (activity as MainActivity).viewModelCurrency
+        initAdapter()
+        binding.ivLeft.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment_to_currencyFragment)
+        }
+        viewModelCurrency.itemsCurrency().observe(viewLifecycleOwner,
+            {
+                    response ->
+                when (response) {
+                    is Resource.Success -> {
+                        hideProgressBar()
+                        response.data?.let { newsResponse ->
+                            settingsAdapter.submitList(newsResponse)
+                        }
+                    }
+                    is Resource.Error -> {
+                        hideProgressBar()
+                        response.data?.let { message ->
+                            toast("Error${message}")
+                        }
 
+                    }
+                    is Resource.Loading -> {
+                        showProgressBar()
+                    }
+                }
+            })
+    }
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+    private fun initAdapter() {
+        settingsAdapter = SettingsAdapter()
+        binding.rvCurrency.adapter = settingsAdapter
     }
 }
